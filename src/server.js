e7a5ea919e48f0a226a54b0c7df4af98d1049511
John Hallman <john_hallman@apple.com>
2018-10-19 15:50:35 -0700

    Adds routes dir

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const path = require('path')
+const routes = require('./controller/routes')
 
 const server = express()
 const PORT = process.env.PORT || 3000
 
 server.use(express.static(path.join(__dirname, '../dist')))
 
-server.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'))
-})
+server.use(routes)
 
 server.listen(PORT, () => {
   console.log('Server is running on port: ', PORT)
