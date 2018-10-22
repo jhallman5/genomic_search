ae474ae5ae9f4983e9670918327241481111b610
John <jhallman5@gmail.com>
2018-10-22 14:51:58 -0700

    Adds React router

--- src/view/index.js
+++ src/view/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Home from './containers/Home'
 
 const App = () =>
-  <div>
-    hi from inside react!
-  </div>
-
+  <BrowserRouter>
+    <Route exact path='/' component={Home} />
+  </BrowserRouter>
 
 ReactDOM.render(<App />, document.getElementById('app'))
