0b4e921bebc69866364cf0d575fe04c344cafa77
John <jhallman5@gmail.com>
2018-10-24 22:02:52 -0700

    Adds some header styling

--- src/view/containers/Search_Results.js
+++ src/view/containers/Search_Results.js
@@ -1,17 +1,46 @@
 import React from 'react'
-import Button from '@material-ui/core/Button';
+import { withStyles, Grid } from '@material-ui/core'
 
 import Search_Bar from './Search_Bar'
 import Varient_Table from './Varient_Table'
 
-export default class Search_Results extends React.Component {
+// <Grid container className={classes.root} spacing={16}>
+//        <Grid item xs={12}>
+//          <Grid container className={classes.demo} justify="center" spacing={Number(spacing)}>
+//            {[0, 1, 2].map(value => (
+//              <Grid key={value} item>
+//                <Paper className={classes.paper} />
+//              </Grid>
+
+class Search_Results extends React.Component {
   render() {
     return (
       <div>
-        Search_Results
-        <Search_Bar />
+        <Grid container spacing={16}>
+          <Grid item xs={3} spacing={8}>
+            <div className={this.props.classes.div}>LOGO</div>
+          </Grid>
+          <Grid item xs={6} spacing={16}>
+            Varient Search
+            <Search_Bar />
+          </Grid>
+        </Grid>
         <Varient_Table />
       </div>
     )
   }
 }
+
+const styles = theme => ({
+  div: {
+    backgroundColor: '#3e5e9c',
+    height: 150,
+    width: 150,
+    color: 'white'
+  },
+  cell: {
+    fontSize: 14
+  }
+})
+
+export default withStyles(styles)(Search_Results)
--- src/view/containers/Varient_Table.js
+++ src/view/containers/Varient_Table.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core'
+import { Table, TableHead, TableBody, TableRow, TableCell, withStyles } from '@material-ui/core'
 
 import VarientTableHeader from '../components/Varient_Table_Header'
 import { fetchGeneData } from '../actions/gene_data'
@@ -14,7 +14,7 @@ class Varient_Table extends React.Component {
   render() {
     return (
       <div>
-        <Table>
+        <Table >
           <VarientTableHeader />
           {
             (this.props.geneData) &&
@@ -22,16 +22,16 @@ class Varient_Table extends React.Component {
                 {
                   this.props.geneData.map( (gene, index) => {
                     return (
-                      <TableRow key={index} >
-                        <TableCell component="th" scope="row">{gene[0]}</TableCell>
-                        <TableCell >{gene[1]}</TableCell>
-                        <TableCell >{gene[2]}</TableCell>
-                        <TableCell >{gene[4]}</TableCell>
-                        <TableCell >{gene[6]}</TableCell>
-                        <TableCell >{gene[7]}</TableCell>
-                        <TableCell >{gene[10]}</TableCell>
-                        <TableCell >{gene[11]}</TableCell>
-                        <TableCell >{gene[12]}</TableCell>
+                      <TableRow className={this.props.classes.row} key={index} >
+                        <TableCell className={this.props.classes.cell} component="th" scope="row">{gene[0]}</TableCell>
+                        <TableCell className={this.props.classes.cell} >{gene[1]}</TableCell>
+                        <TableCell className={this.props.classes.cell} >{gene[2]}</TableCell>
+                        <TableCell className={this.props.classes.cell} >{gene[4]}</TableCell>
+                        <TableCell className={this.props.classes.cell} >{gene[6]}</TableCell>
+                        <TableCell className={this.props.classes.cell} >{gene[7]}</TableCell>
+                        <TableCell className={this.props.classes.cell} >{gene[10]}</TableCell>
+                        <TableCell className={this.props.classes.cell} >{gene[11]}</TableCell>
+                        <TableCell className={this.props.classes.cell} >{gene[12]}</TableCell>
                       </TableRow>
                     )
                   })
@@ -43,6 +43,17 @@ class Varient_Table extends React.Component {
     )
   }
 }
+
+const styles = theme => ({
+  row: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: '#F0F0F0',
+    }
+  },
+  cell: {
+    fontSize: 14
+  }
+})
 
 const mapStateToProps = state => {
   return {
@@ -56,4 +67,5 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Varient_Table))
+export default withRouter(withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(Varient_Table))
+)
