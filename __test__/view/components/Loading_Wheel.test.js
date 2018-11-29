b570cb985eaa29d0e91d526c983bf77efcd9a79a
John Hallman <john_hallman@apple.com>
2018-11-29 13:00:09 -0800

    Adds more robust tests for Loading Wheel

--- __test__/view/components/Loading_Wheel.test.js
+++ __test__/view/components/Loading_Wheel.test.js
@@ -1,27 +1,74 @@
 import React from 'react';
 import { LoadingWheel } from '../../../src/view/components/Loading_Wheel';
-import renderer from 'react-test-renderer';
-import { MuiThemeProvider } from '@material-ui/core/'
-import { shallow, mount } from 'enzyme'
+import { shallow } from 'enzyme';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const props = {
-  classes: {
-    progress: {
-      color: '#39B7AA',
-      position: 'fixed',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)'
+describe('LoadingWheel', () =>  {
+  const props = {
+    classes: {
+      progress: {
+        color: '#39B7AA',
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'
+      }
     }
   }
-}
-test('LoadingWheel renders', () => {
-  const wrapper = shallow(<LoadingWheel {...props} />);
-  const component = renderer.create(wrapper);
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+
+  describe('Lifecycle methods', () =>  {
+    test('renders', () => {
+      const wrapper = shallow(<LoadingWheel {...props} />);
+      expect(wrapper.exists()).toBe(true);
+    })
+
+    test('componentDidMount sets setInterval', () => {
+      jest.useFakeTimers();
+      const wrapper = shallow(<LoadingWheel {...props} />);
+      expect(setInterval).toHaveBeenCalledTimes(1);
+    })
+
+    test('componentWillUnmount removes setInterval', () => {
+      jest.useFakeTimers();
+      const wrapper = shallow(<LoadingWheel {...props} />);
+      expect(setInterval).toHaveBeenCalledTimes(1);
+      wrapper.unmount();
+      expect(clearInterval).toHaveBeenCalledTimes(1);
+    })
+  })
+
+  describe('Progress Method', () =>  {
+    test('increments state by 1', () => {
+      const wrapper = shallow(<LoadingWheel {...props} />);
+      wrapper.instance().progress();
+      expect(wrapper.state().completed).toBe(1);
+    })
+
+    test('Multiple calls increments state by 1 each time', () => {
+      const wrapper = shallow(<LoadingWheel {...props} />);
+      wrapper.instance().progress();
+      expect(wrapper.state().completed).toBe(1);
+      wrapper.instance().progress();
+      expect(wrapper.state().completed).toBe(2);
+      wrapper.instance().progress();
+      expect(wrapper.state().completed).toBe(3);
+    })
+
+    test('State completes at 1.5 seconds', () => {
+      jest.useFakeTimers();
+      const wrapper = shallow(<LoadingWheel {...props} />);
+      jest.advanceTimersByTime(1500);
+      expect(wrapper.state().completed).toBe(100);
+    })
+
+    test('State increments consistently', () => {
+      jest.useFakeTimers();
+      const wrapper = shallow(<LoadingWheel {...props} />);
+      jest.advanceTimersByTime(750);
+      expect(wrapper.state().completed).toBe(50);
+    })
+  })
 })
