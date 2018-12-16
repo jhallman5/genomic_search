3e87aa1f6007caa0d1406ebb791e97410c21b469
John <jhallman5@gmail.com>
2018-12-16 14:59:59 -0800

    connects AWS RDS

--- src/model/gene_model.js
+++ src/model/gene_model.js
@@ -1,11 +1,33 @@
 const { selectFullGene, selectPossibleGeneNames } = require('./db/queries')
 
+const { Pool, Client } = require('pg')
+
+const client = new Client({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+})
+client.connect()
+
 const getFullGene = (gene) => {
-  return new Promise((resolve, reject) => {
-    const geneList = selectFullGene(gene).filter(x => x.length > 1)
-    resolve({'status': 'Success', 'geneList': geneList})
+  client.query("SELECT * FROM gene_table WHERE gene_name='ENG' LIMIT 1;", (err, res) => {
+    console.log(err, res)
+    client.end()
   })
+  // return new Promise((resolve, reject) => {
+  //   const geneList = selectFullGene(gene).filter(x => x.length > 1)
+  //   resolve({'status': 'Success', 'geneList': geneList})
+  // })
 }
+
+// const getFullGene = (gene) => {
+//   return new Promise((resolve, reject) => {
+//     const geneList = selectFullGene(gene).filter(x => x.length > 1)
+//     resolve({'status': 'Success', 'geneList': geneList})
+//   })
+// }
 
 const getPossibleGeneNames = (input) => {
   return new Promise((resolve, reject) => {
